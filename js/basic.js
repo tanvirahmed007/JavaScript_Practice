fe7bce26e44e08ec6c7daa14d63c173f0c9a529a
Tanvir Ahmed <tanvir15-9909@diu.edu.bd>
2022-09-04 00:01:52 +0600

    function and arrow function, onclick

--- js/basic.js
+++ js/basic.js
@@ -1,9 +1,48 @@
 
-console.log("Hello World");
+// intro
 
-// variable declaration
-var a = 10;
-var b = 20;
-var c = a + b;
+document.getElementById("heading").innerHTML = "Hello World!";
+document.getElementById("heading").style.color = "red";
+document.getElementById("heading").style.backgroundColor = "yellow";
 
-console.log(c);
+// write() method
+
+document.write("Hello World!");
+
+// alert() method
+
+// window.alert("Hello World! Welcome to JavaScript");
+alert("Hello World! Welcome to JavaScript");
+
+// console.log() method
+
+console.log("Hello World! Welcome to JavaScript");
+
+// function call
+
+// function sum(){
+//     var a = 10;
+//     var b = 20;
+//     var c = a+b;
+//     console.log("the sum of a & b = ", c);
+// }
+
+// arrow function
+const sum = () =>{
+    var a = 80;
+    var b = 20;
+    var c = a+b;
+    console.log("the sum of a & b = ", c);
+};
+
+
+const call = () =>{
+    document.getElementById("title").innerHTML = "Tanvir Ahmed";
+    document.getElementById("title").style.color = "white";
+    document.getElementById("title").style.backgroundColor = "black";
+}
+
+const remove = () =>{
+    document.getElementById("title").style.display = "none";
+}
+
