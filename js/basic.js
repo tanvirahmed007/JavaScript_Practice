303de235e2ef8320398853497e11de181bc73dd3
Tanvir Ahmed <tanvir15-9909@diu.edu.bd>
2022-09-07 23:50:13 +0600

    Object

--- js/basic.js
+++ js/basic.js
@@ -46,3 +46,70 @@ const remove = () =>{
     document.getElementById("title").style.display = "none";
 }
 
+
+
+
+// Object
+
+const human = {
+    name: "Tanvir Ahmed",
+    age: 25,
+    city: "Dhaka",
+    country: "Bangladesh",
+    education: "B.Sc in CSE",
+    job: "Web Developer",
+    salary: 25000,
+    married: false,
+};
+console.log(human);
+console.log(human.salary);
+
+
+// Array
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(numbers);
+console.log(numbers[3]);
+
+numbers.push(50);
+numbers.push("Tanvir");
+console.log(numbers);
+console.log(numbers.length);
+
+
+
+// object
+
+const car = {
+    name: "BMW",
+    model: "X5",
+    color: "black",
+    price: 5000000,
+    engine: "V8",
+    year: 2020,
+    country: "Germany",
+    owner: "Tanvir Ahmed",
+    phone: 01700000000,
+    address: "Dhaka, Bangladesh",
+    married: false,
+    total: function(){
+        return this.name+ " " +this.model+ " " +this.color;
+    }
+};
+console.log(car.total());
+console.log(car);
+
+delete car.owner;
+console.log(car);
+
+// nested object
+const myObj = {
+    name: "John",
+    age: 30,
+    cars: [
+      {name:"Ford", models:["Fiesta", "Focus", "Mustang"]},
+      {name:"BMW", models:["320", "X3", "X5"]},
+      {name:"Fiat", models:["500", "Panda"]}
+    ]
+  } 
+  console.log(myObj);
